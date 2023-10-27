eb1f2934380d3492cb9e0b636b47559c5660968c
atari-monk <atari.monk1@gmail.com>
2023-10-28 01:01:53 +0200

    problem with script

--- js_script/copy-package.js
+++ js_script/copy-package.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 
-function copyFileToBuild(sourceFile, targetDir, workingDir) {
-  const projectRootDir = path.resolve(workingDir)
+function copyFileToBuild(sourceFile, targetDir) {
+  const scriptDir = __dirname
+  const projectRootDir = path.resolve(scriptDir, '.')
+  console.log('scriptDir:', projectRootDir)
   const sourcePath = path.join(projectRootDir, sourceFile)
+  console.log('sourcePath:', sourcePath)
   const targetPath = path.join(
     projectRootDir,
     targetDir,
     path.basename(sourceFile)
   )
+  console.log('targetPath:', targetPath)
 
   if (!fs.existsSync(path.dirname(targetPath))) {
     fs.mkdirSync(path.dirname(targetPath), { recursive: true })
@@ -19,4 +23,18 @@ function copyFileToBuild(sourceFile, targetDir, workingDir) {
   console.log(`Copied ${sourcePath} to ${targetPath}`)
 }
 
-module.exports = copyFileToBuild
+function copyFileToBuild2(sourcePath, targetPath) {
+  const sp = path.resolve(sourcePath)
+  const tp = path.resolve(targetPath)
+  console.log('sourcePath2', sp)
+  console.log('targetPath2', tp)
+  if (!fs.existsSync(tp)) {
+    fs.mkdirSync(tp, { recursive: true })
+  }
+
+  fs.copyFileSync(sp, tp)
+
+  console.log(`Copied ${sp} to ${tp}`)
+}
+
+module.exports = { copyFileToBuild, copyFileToBuild2 }
